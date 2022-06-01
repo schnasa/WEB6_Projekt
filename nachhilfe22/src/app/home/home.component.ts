3740254047738836aa8ec880f8ce292b782bc4dd
schnasa <schnallinger.sarah@gmail.com>
2022-06-01 22:10:59 +0200

    updateProject

--- nachhilfe22/src/app/home/home.component.ts
+++ nachhilfe22/src/app/home/home.component.ts
@@ -3,9 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'bs-home',
   template: `
-    <div>
-      <h1>Home</h1>
-      <p>Such das richtige Angebot für dich!</p>
+    <div class="ui main container">
+      <h1>Nachhilfe für ALLE!</h1>
+      <p>Such das richtige Angebot für dich! Wir bieten verschiedene Nachhilfeangebote für unterschiedliche Levels.
+        Von Volksschule über neue Mittelschule und Oberstufe, bishin zur Matura, Bachelore, Master und Doktorat.</p>
       <a routerLink="../lvas" class="ui red button">
         Alle Angebote ansehen
       </a>
